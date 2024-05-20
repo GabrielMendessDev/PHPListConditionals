1b95d876b52e72255c78304a9b027a8e1fd1e0f1
Gabriel Mendes <lucasgabriel16161@gmail.com>
2024-05-20 18:32:32 -0300

    questao02.php concluída

---
+++ questao02.php
@@ -0,0 +1,24 @@
+<?php
+
+function pegarValor($num){
+    return $num = isset($_GET[$num]);
+}
+
+$num1 = pegarValor('n1');
+$num2 = pegarValor('n2');
+$num3 = pegarValor('n3');
+
+if ($num1 === null || $num2 === null || $num3 === null) {
+    echo "Por favor, forneça os três números como parâmetros GET na URL. Exemplo: ?num1=10&num2=20&num3=5";
+    exit;
+}
+
+echo "Números fornecidos na URL: $num1, $num2 e $num3";
+
+if ($num1 > $num2 && $num1 > $num3){
+    echo "O $num1 é o maior.";
+}else if($num2 > $num1 && $num2 > $num3){
+    echo "O $num2 é o maior.";
+}else{
+    echo "O $num3 é o maior.";
+}
