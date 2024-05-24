f91d6c8bcd8200294884bc3eeb203a9527898d5d
Gabriel Mendes <lucasgabriel16161@gmail.com>
2024-05-24 15:14:24 -0300

    questao05.php concluída

---
+++ questao05.php
@@ -0,0 +1,14 @@
+<?php
+
+$ano = isset($_GET['a']);
+
+if ($ano === null) {
+    echo "Por favor, forneça o ano. Exemplo: ?a=2005";
+    exit;
+}
+
+if (($ano % 400 == 0 || $ano % 4 == 0) && $ano % 100 != 0){
+    echo "O ano $ano é bissexto.";
+}else{
+    echo "O ano $ano não é bissexto.";
+}
